c2bb22f313cb76134d3a51ad564a4bbb0cd6ce66
dcojp <109931434+dcojp@users.noreply.github.com>
2023-01-04 19:06:56 +0900

    JSによるtrの追加

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import score from 'https://dcojp.github.io/practice_html/score.json' assert {type: 'json'}
 
+const tr = document.createElement('tr');
+const th = document.createElement('th');
+
+const $tab = document.getElementById('js-tab');
+
+$tab.appendChild(tr);
+
 console.log(score[0].title);
